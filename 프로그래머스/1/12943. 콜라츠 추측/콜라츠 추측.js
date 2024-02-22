74e60113b6cefd2c746f4affaacfab7f2a01066b
Jean Young Park <Jean.Young.Park.96@gmail.com>
2024-02-22 09:20:39 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.11 ms, Memory: 33.2 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,23 +1,36 @@
 function solution(num) {
     var answer = 0;
     
-    function fn(n){
-        if(answer > 500) {
-            answer = -1;
-            return;
-        }
+//     function fn(n){
+//         if(answer > 500) {
+//             answer = -1;
+//             return;
+//         }
             
-        if(n === 1) return;
+//         if(n === 1) return;
         
-        answer += 1;
+//         answer += 1;
                
-        if(n % 2 === 1) {
-            fn(n * 3 + 1);
+//         if(n % 2 === 1) {
+//             fn(n * 3 + 1);
+//         }else{
+//             fn(n / 2)
+//         }
+//     }
+    
+    var tmp = num;
+    
+    while(tmp !== 1 && answer !== 500){
+        answer += 1;
+        
+        if(tmp % 2 === 1) {
+            tmp = tmp * 3 + 1;
         }else{
-            fn(n / 2)
+            tmp = tmp / 2;
         }
     }
     
-    fn(num)
+    if(answer === 500) answer = -1;
+    // fn(num)
     return answer;
 }
