43a536473eda71b20c42a386004c1c81e33f596a
Jean Young Park <Jean.Young.Park.96@gmail.com>
2024-02-25 21:39:08 +0900

    [level 2] Title: 피로도, Time: 0.28 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.js
+++ 프로그래머스/2/87946. 피로도/피로도.js
@@ -1,21 +1,20 @@
 function solution(k, dungeons) {
-    var answer = -1;
+    let answer = -1;
     
-    function fn(cnt, leftDungeons, hp){
+    const fn = (cnt, leftDungeons, hp) => {
         if(leftDungeons.length === 0) {
             answer = Math.max(answer, cnt);
             return;
         }
         
-        for(var i = 0; i < leftDungeons.length; i++){
-            var newDungeons = leftDungeons.slice(0, i).concat(leftDungeons.slice(i+1));
+        for(let i = 0; i < leftDungeons.length; i++){
+            const newDungeons = leftDungeons.slice(0, i).concat(leftDungeons.slice(i+1));
             
             if(hp >= leftDungeons[i][0]){
                 fn(cnt+1, newDungeons, hp-leftDungeons[i][1]);
             }else{
                 fn(cnt, newDungeons, hp);
             }
-            
         }
         
     }
