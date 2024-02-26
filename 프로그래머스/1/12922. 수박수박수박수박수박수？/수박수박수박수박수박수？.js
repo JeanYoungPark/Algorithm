51992ada67fff6cb28e912097722b9774724b154
Jean Young Park <Jean.Young.Park.96@gmail.com>
2024-02-27 08:49:59 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -1,12 +1,6 @@
 function solution(n) {
     var answer = '';
     var str = '수박';
-    var i = Math.floor(n/2);
-    
-    while(i--){
-        answer += str;
-    }
-    
-    answer += n % 2 ? '수' : '';
+    answer = str.repeat(n/2)+( n % 2 ? '수' : '');
     return answer;
 }
