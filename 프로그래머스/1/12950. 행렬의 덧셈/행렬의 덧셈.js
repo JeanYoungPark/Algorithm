db38874fc7396a266d99a406ffdd530024d51baa
Jean Young Park <Jean.Young.Park.96@gmail.com>
2024-03-01 13:38:06 +0900

    [level 1] Title: 행렬의 덧셈, Time: 8.34 ms, Memory: 67.6 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -4,7 +4,7 @@ function solution(arr1, arr2) {
     for(var i = 0; i < arr1.length; i++){
         var innerArr = [];
         
-        for(var j = 0; j < arr1[0].length; j++){
+        for(var j = 0; j < arr1[i].length; j++){
             innerArr.push(arr1[i][j]+arr2[i][j]);
         }
         
