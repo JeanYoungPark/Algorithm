e3afb0426716de24697c3679e1252ae86fcd5301
Jean Young Park <Jean.Young.Park.96@gmail.com>
2024-03-01 21:33:14 +0900

    [level 2] Title: 모음 사전, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.js
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.js
@@ -1,12 +1,4 @@
 function solution(word) {
-    const vowels = ['A', 'E', 'I', 'O', 'U'];
-    const counts = [781, 156, 31, 6, 1];
-    let answer = 0;
-
-    for (let i = 0; i < word.length; i++) {
-        const index = vowels.indexOf(word[i]);
-        answer += (index * counts[i]) + 1;
-    }
-
+    var answer = 0;
     return answer;
 }
