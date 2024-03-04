3935242631885eaea3d43ae0e323bc420bf2d578
Jean Young Park <Jean.Young.Park.96@gmail.com>
2024-03-04 13:28:07 +0900

    [level 1] Title: 시저 암호, Time: 38.20 ms, Memory: 38.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.js
@@ -0,0 +1,27 @@
+function solution(s, n) {
+    var answer = '';
+    var arr = s.split('').map(data => data.charCodeAt());
+    
+    for(var i = 0; i < arr.length; i++){
+        if(arr[i] >= 65 && arr[i] <= 90){
+            // 대문자인경우
+            arr[i] += n;
+            
+            if(arr[i] > 90){
+                arr[i] -= 26
+            }
+         }else if(arr[i] >= 97 && arr[i] <= 122){
+             // 소문자인경우
+             arr[i] += n;
+             
+             if(arr[i] > 122){
+                arr[i] -= 26
+            }
+             console.log(arr[i])
+         }
+        
+        answer += String.fromCharCode(arr[i]);
+    }
+    
+    return answer;
+}
