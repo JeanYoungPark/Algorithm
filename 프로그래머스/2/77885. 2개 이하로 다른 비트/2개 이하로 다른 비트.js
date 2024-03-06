be48bb473035f08c651be39858d599a81c796ece
Jean Young Park <Jean.Young.Park.96@gmail.com>
2024-03-06 15:35:05 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 205.72 ms, Memory: 59 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.js
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.js
@@ -6,11 +6,14 @@ function solution(numbers) {
     }
     
     function fn(num){
+        //짝수라면 제일 왼쪽의 비트가 0일테니 +1
         if(num % 2 === 0) return num+1;
         else{
             var bit = num.toString(2);
+            // 가장 뒤의 인덱스의 있는 01을 뒤집어 10으로 만들면 다음으로 가장 큰 수가 된다. (최대 2자리수만 바꿀 수 있음)
             var idx = bit.lastIndexOf('01');
             
+            // 01이 없다면 1밖에 없는 이진수라는 뜻
             if(idx === -1){
                 bit = '10'+bit.slice(1);
             }else{
--- 프로그래머스/2/77885. 2개 이하로 다른 비트/README.md
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/README.md
@@ -1,10 +1,10 @@
 # [level 2] 2개 이하로 다른 비트 - 77885 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77885) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77885#) 
 
 ### 성능 요약
 
-메모리: 58.8 MB, 시간: 168.01 ms
+메모리: 59 MB, 시간: 205.72 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 06일 15:32:02
+2024년 03월 06일 15:35:04
 
 ### 문제 설명
 
