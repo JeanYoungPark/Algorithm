cd34dc6ebb9eff76f8f10f37ea1027b7c056a882
Jean Young Park <Jean.Young.Park.96@gmail.com>
2024-03-17 19:28:09 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.js
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.js
@@ -1,19 +1,14 @@
 function solution(number, k) {
-  const stack = [];
-  let count = 0;
-
-  for (const item of number) {
-    while (count < k && stack[stack.length - 1] < item) {
-      stack.pop();
-      count += 1;
+    var answer = '';
+    var num = {};
+    var tmp = '';
+    
+    for(var i = 0; i < number.length - k; i++){
+        for(var j = i+1; j < number.length - k; j++){
+            console.log(number.substr(i, 1),number.substr(j, number.length - k - 1));
+        }
     }
-    stack.push(item);
-  }
-
-  while (count < k) {
-    stack.pop();
-    count += 1;
-  }
-
-  return stack.join("");
+    
+    
+    return answer;
 }
